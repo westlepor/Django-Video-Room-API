8549c363965af5bb069789ead0d67643a825c86e
westlepor <westlepor984@gmail.com>
2020-07-26 19:22:40 -0700

    add simple api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Users, Rooms
+from .serializers import UserSerializer, RoomSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+  lookup_field='user_id'
+  queryset = Users.objects.all()
+  serializer_class = UserSerializer
+
+class RoomViewSet(viewsets.ModelViewSet):
+  lookup_field='room_id'
+  queryset = Rooms.objects.all()
+  serializer_class = RoomSerializer
